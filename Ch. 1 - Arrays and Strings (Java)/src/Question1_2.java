a19d742a360241b771a98ee6b0322cfd3f5c31af
SuperLinguini <milind.lingineni@gmail.com>
2017-05-23 19:58:03 -0400

    O(n) solution to Question 1.2

--- Ch. 1 - Arrays and Strings (Java)/src/Question1_2.java
+++ Ch. 1 - Arrays and Strings (Java)/src/Question1_2.java
@@ -12,19 +12,50 @@ public class Question1_2 {
             boolean anagram = isPermutation(word1, word2);
             System.out.println(word1 + ", " + word2 + ": " + anagram);
         }
+        for (String[] pair : pairs) {
+            String word1 = pair[0];
+            String word2 = pair[1];
+            boolean anagram = isPermutation2(word1, word2);
+            System.out.println(word1 + ", " + word2 + ": " + anagram);
+        }
     }
 
+    /*
+     * Sorts the string by converting it to a char array and then back to
+     * a string. O(n log n) solution
+     */
     public static boolean isPermutation(String first, String second) {
         if (first.length() != second.length()) {
             return false;
         }
-        return getSortedCharArr(first).equals(getSortedCharArr(second));
+        return getSortedString(first).equals(getSortedString(second));
     }
 
-    private static String getSortedCharArr(String str) {
+    private static String getSortedString(String str) {
         char[] charArray = str.toCharArray();
         Arrays.sort(charArray);
         return new String(charArray);
+    }
 
+    /*
+     * O(f + s) solution where f,s are lengths of first and second respectively
+     * using a counter for each character. Assumed to be ASCII
+     */
+    private static boolean isPermutation2(String first, String second) {
+        if (first.length() != second.length()) {
+            return false;
+        }
+
+        int[] asciiLetterCount = new int[128];
+        for (int i = 0; i < first.length(); i++) {
+            asciiLetterCount[first.charAt(i)]++;
+        }
+
+        for (int j = 0; j < second.length(); j++) {
+            if (--asciiLetterCount[second.charAt(j)] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
